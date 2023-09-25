682b8e00d8babcab1b9b79ef6fa81ae3a4fde8b1
Ruzana khan <90472334+Ruzana-khan@users.noreply.github.com>
2023-09-25 11:16:29 +0600

    first commit

---
+++ quiz.py
@@ -0,0 +1,68 @@
+score = 0
+print("quiz app")
+print("what is the capital of bangladesh?")
+option = "1.khulna 2.dhaka 3.chottogram"
+print(option)
+
+answer = input("press 1 or 2 or 3 : ")
+
+if answer =="2" :
+  print("correct")
+  score = score+10
+else: 
+  score = score-5
+  print("incorrect")
+
+
+print("what is light speed?")
+option = "1. 2.99 792 458 m/s 2.39m/s 3.3.8 m/s"
+print(option)
+
+answer = input("press 1 or 2 or 3 : ")
+
+if answer =="1" :
+  score = score+10
+  print("correct")
+else: 
+  score = score-5
+  print("incorrect")
+
+print("what is the capital of japan?")
+option = "1.beijing 2.dhaka 3.Tokyo"
+print(option)
+
+answer = input("press 1 or 2 or 3 : ")
+
+if answer =="3" :
+  score = score+10
+  print("correct")
+else: 
+  score = score-5
+  print("incorrect")
+
+print("what is the capital of vietnam?")
+option = "1.pyong hyong 2.seoul 3.hanoi"
+print(option)
+
+answer = input("press 1 or 2 or 3 : ")
+
+if answer =="3" :
+  score = score+10
+  print("correct")
+else: 
+  score = score-5
+  print("incorrect")
+
+
+print("what is the most powerful computer?")
+option = "1.Frontier 2.Fugaku 3.super computer"
+print(option)
+
+answer = input("press 1 or 2 or 3 : ")
+
+if answer =="1" :
+  score = score+10
+  print("correct")
+else: 
+  score = score-5
+  print("incorrect")
